2a87e845f78f1d11b290bea970347db9de8ca76a
Kevin Tan Yulo <yulokevin@gmail.com>
2021-12-01 20:15:15 -0800

    refactor

--- fetcher.js
+++ fetcher.js
@@ -5,15 +5,14 @@ const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 let rl = readline.createInterface({ input, output });
 
-
 request(uInput[0], (error, response, body) => {
   //file already exists
+  
   let wantToContinue = true;
- 
   fs.readFile(uInput[1], 'utf8' , (err, data) => {
+    // console.log(err, data)
     if (err) {
-      // console.log ("creating");
-      return;
+      writeFile(body);
     } else {
       rl.question("Already exists. Rewrite? Y/N: ", (answer) => {
         if (answer === "N" || answer === "n") {
@@ -22,28 +21,28 @@ request(uInput[0], (error, response, body) => {
         } else {
           console.log("Rewriting");
           wantToContinue = true;
-          
-          if (wantToContinue === true) {
-            console.log("made it");
-    
-            fs.writeFile(uInput[1], body, err => {
-              if (err) {
-                return console.log("error");
-              } else {
-    
-                const stats = fs.statSync(uInput[1]);
-                const fileSizeInBytes = stats.size;
-                
-                console.log(`Downloaded and saved ${fileSizeInBytes} to ${uInput[1]} `);
-              }
-            //file written successfully
-            }); //fs write file
-          } //if
+        }
+        if (wantToContinue === true) {
+          writeFile(body);
         }
         rl.close();
-      });  //end question
-      //   }//end els
-      // }) // readfile async
-    } //else
-  });//path exist
-});//request
+      });
+    }
+    // console.log("want to write");
+  });  //end question
+    
+});
+
+const writeFile = (body) => {
+
+  console.log("made it");
+  fs.writeFile(uInput[1], body, err => {
+    if (err) {
+      return console.log("error");
+    } else {
+      const stats = fs.statSync(uInput[1]);
+      const fileSizeInBytes = stats.size;
+      console.log(`Downloaded and saved ${fileSizeInBytes} to ${uInput[1]}`);
+    }
+  });
+};
